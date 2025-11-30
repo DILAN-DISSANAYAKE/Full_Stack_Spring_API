35d8011755be0bd16f28c9e0f364ef72a810dd41
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-11-30 14:59:49 +0530

    feat: add ProductRepository interface for Product entity persistence

---
+++ src/main/java/edu/icet/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package edu.icet.repository;
+
+import edu.icet.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
