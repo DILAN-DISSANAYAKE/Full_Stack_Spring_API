fa2f2658a77c82f6406b52f0d58640ab69ece114
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-12-18 22:48:08 +0530

    fix(entity): Rename unitPrice to price in OrderDetails

--- src/main/java/edu/icet/model/entity/OrderDetails.java
+++ src/main/java/edu/icet/model/entity/OrderDetails.java
@@ -19,6 +19,6 @@ public class OrderDetails {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product productId;
-    private double unitPrice;
+    private double price;
     private int qty;
 }
